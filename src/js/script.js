1f9b60ea022af843f779bdb02ca8d8f4e8c8f0b9
ElisaSaltini <elisa.saltini@edu.univ-fcomte.fr>
2025-11-07 11:23:29 +0100

    TP1 fin

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-// TODO
+// changer la couleur du texte en rouge
+const AMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+AMettreEnRouge.style.color = "red"
+
+//changer en rouge quand click
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+enRougeSuiteAClick.addEventListener("click", (evt)=>{
+    enRougeSuiteAClick.style.color = "red"
+}); 
+
+//changer en rouge seconds titres quand click
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color = "red";
+    });
+}); 
